ddc25b6d28b42c07b2fa01924773cdbb24932ffd
AllePansan <alexpjunior@terra.com.br>
2017-09-26 21:11:06 -0300

    Splash Added
    Addition of an Splash screen that "supposed to" wait an connection of the Arduino ESP8266 board

---
+++ app/src/main/java/com/example/alexp/controleleds/ConnectionRunnable.java
@@ -0,0 +1,12 @@
+package com.example.alexp.controleleds;
+
+/**
+ * Created by alexp on 25/09/2017.
+ */
+
+public class ConnectionRunnable implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
